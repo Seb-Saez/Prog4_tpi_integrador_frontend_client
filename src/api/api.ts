6370c190232fcdffe2471e29063a96b7becd5537
Seba Saez <sebastiansaez44@gmail.com>
2026-05-18 11:00:37 -0300

    Se sube rama devel

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export const API_BASE = import.meta.env.VITE_API_URL;
 
+// se sube el .env para ejecutar los llamados a la api, sino es http://localhost:8000
+
 const api = axios.create({
   baseURL: API_BASE,
   headers: {
